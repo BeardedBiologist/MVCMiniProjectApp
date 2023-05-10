6d79e0479b99e6c143599fc9089bd37174000675
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-10 10:05:45 +0200

    init commit -- project setup

---
+++ MVCMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMiniProject.Controllers
+{
+    public class AddressController : Controller
+    {
+        // GET: AddressController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        // GET: AddressController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST:AddressController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
